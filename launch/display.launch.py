7837d592bc766abf96847f7c0ba1e0e77499656c
JungJooYoo <d21141302@365.mokpo.ac.kr>
2023-11-08 13:20:14 +0900

    first commit

--- launch/display.launch.py
+++ launch/display.launch.py
@@ -1,4 +1,5 @@
 import os
+
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.substitutions import LaunchConfiguration
@@ -10,10 +11,10 @@ from launch_ros.actions import Node
 def generate_launch_description():
     pkg_gazebo_ros = FindPackageShare(package='gazebo_ros').find('gazebo_ros')
     pkg_tb3_gazebo = FindPackageShare(package='turtlebot3_gazebo').find('turtlebot3_gazebo')
-    pkg_tb3_bringup = FindPackageShare(package='turtlebot3_bringup').find('turtlebot3_bringup')
+    _pkg_tb3_bringup = FindPackageShare(package='turtlebot3_bringup').find('turtlebot3_bringup')
     
     pkg_share = FindPackageShare(package='follow_line').find('follow_line')
-    default_world_path = os.path.join(pkg_share, 'maps', 'lfm4.world')
+    default_world_path = os.path.join(pkg_share, 'maps', 'lfm2_nowalls.world')
 
     use_sim_time = LaunchConfiguration('use_sim_time', default='true')
     x_pose = LaunchConfiguration('x_pose', default='0.0')
